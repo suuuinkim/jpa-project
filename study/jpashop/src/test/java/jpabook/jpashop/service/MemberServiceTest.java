2c6b0cde3cf17c6d58e776748b4a851085c9cfbd
suuuinkim <suchae7@naver.com>
2023-03-12 23:45:51 +0900

    [추가] 회원 도메인 개발

--- study/jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ study/jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -6,8 +6,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
 
 import static org.junit.Assert.*;
 
@@ -18,7 +21,10 @@ public class MemberServiceTest {
 
     @Autowired MemberService memberService;
     @Autowired MemberRepository memberRepository;
+    @Autowired EntityManager em;
+
     @Test
+    @Rollback(false)
     public void 회원가입() throws Exception{
         //given
         Member member = new Member();
@@ -28,16 +34,29 @@ public class MemberServiceTest {
         Long saveId = memberService.join(member);
 
         //then
+//        em.flush(); // 영속성 컨테이너에 있는 내용을 데이터베이스에 반영
         assertEquals(member, memberRepository.findOnd(saveId));
     }
 
-    @Test
+    @Test(expected = IllegalStateException.class)
     public void 중복_회원_예외() throws Exception{
         //given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
 
         //when
+        memberService.join(member1);
+
+        try{
+            memberService.join(member2); // 예외가 발생해야됨!!!
+        } catch (IllegalArgumentException e){
+            return;
+        }
 
         //then
-
+        fail("예외가 발생해야 한다");
     }
 }
