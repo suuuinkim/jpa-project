5e89dd8d3b43a5d2edc13a63d01d85c9dca5976b
suuuinkim <suchae7@naver.com>
2023-03-21 00:01:46 +0900

    [추가] 변경감지와 병함

--- study/jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
+++ study/jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -18,6 +18,13 @@ public class ItemRepository {
         if(item.getId() == null){ // item 값이 없음! 새로 생성하는 것임
             em.persist(item);
         }else{ // update와 비슷하다고 생각하면 됨
+            // 병합은 준영속 상태의 엔티티를 영속 상태로 변경할 때 사용하는 기능이다.
+            /* item에 넘어온 값으로 바꿔치기를 해버림
+               병합은 조심해야됨!!!!
+               왜? 모든 속성이 다 바뀌기 때문!!!
+               만약에 값이 없으면 null로 다 바뀜
+               merge는 사용을 지양하고 불편하더라도 변경감지를 사용하는 것이 좋음
+            */
             em.merge(item);
         }
     }
--- study/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ study/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -24,18 +24,22 @@ public class ItemService {
      * 변경 감지 기능
      */
     @Transactional
-    public void updateItem(Long itemId, Book param){
+    // public void updateItem(Long itemId, String name, int price, int stockQuantity){ // 파라미터가 많을경우 DTO를 만들어서 넘겨주는 것이 좋음
+    public void updateItem(Long itemId, UpdateItemDto itemDto){
         Item findItem = itemRepository.findOne(itemId); // 영속상태
-        findItem.setPrice(param.getPrice());
-        findItem.setName(param.getName());
-        findItem.setStockQuantity(param.getStockQuantity()); // 값 세팅
+        // findItem.change(price, name, stockQuantity); 다 set으로 하는것 보다 의미 있는 메소드를 만들어 두는 것이 좋음
+        findItem.setPrice(itemDto.getPrice());
+        findItem.setName(itemDto.getName());
+        findItem.setStockQuantity(itemDto.getStockQuantity()); // 값 세팅
         // ==== transactional에 의해서 커밋이 됨 ===
         // 그럼 jpa는 플러시를 날림 -> 영속성컨테이너에서 변경된 애가 뭔지 다 찾음!!!
         // 뭐가 바꼇네???
         // 변경감지에 의해서 update 침
+
+
     }
 
-    // 병합은 준영속 상태의 엔티티를 영속 상태로 변경할 때 사용하는 기능이다. 
+
     public List<Item> findItems(){
         return itemRepository.findAll();
     }
