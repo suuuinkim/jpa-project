de7e52f820cfa63722a9bb7ed1a991ac1304a809
suuuinkim <suchae7@naver.com>
2023-03-29 20:04:49 +0900

    [추가] 주문조회 v2 : 엔티티를 DTO로 변환

--- study/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ study/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -1,14 +1,16 @@
 package jpabook.jpashop.api;
 
-import jpabook.jpashop.domain.Order;
-import jpabook.jpashop.domain.OrderItem;
-import jpabook.jpashop.domain.OrderSearch;
+import jpabook.jpashop.domain.*;
 import jpabook.jpashop.repository.OrderRepository;
+import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -32,5 +34,62 @@ public class OrderApiController {
 
         }
         return all;
+    }
+
+    /**
+     * 껍데이 엔티티만 노출하면 안되는 것이 아니라, 속에 있는 것까지 외부에 노출하면 안됨!
+     * 쿼리가 너무 많이 사용된다는 단점이 있다
+     */
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2(){
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<OrderDto> collect = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+
+        return collect;
+    }
+
+    @Getter
+    static class OrderDto{
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        // private List<OrderItem> orderItems;
+        private List<OrderItemDto> orderItems;
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+
+            /**
+             * DTO안에 엔티티가 있으면 안됨!
+             * 엔티티가 외부에 노출되어 버림 (의존을 완전히 끊어야됨)
+             * 귀찮더라도 OrderITem조차도 DTO로 바꿔야됨!!!
+             */
+            // order.getOrderItems().stream().forEach(o -> o.getItem().getName());
+
+            orderItems = order.getOrderItems().stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Data
+    static class OrderItemDto{
+
+        private String itemName; // 상품명
+        private int orderPrice;
+        private int count;
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
     }
 }
