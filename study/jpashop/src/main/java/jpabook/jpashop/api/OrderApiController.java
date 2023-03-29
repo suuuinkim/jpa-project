3114a5ec975c697c29e26bca161f51924ff66de1
suuuinkim <suchae7@naver.com>
2023-03-29 19:20:02 +0900

    [추가] 주문 조회 V! : 엔티티 직접 노출

---
+++ study/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderSearch;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+    private final OrderRepository orderRepository;
+
+    /**
+     * Order 엔티티를 노출하는 방법은 좋지 않음
+     * @return
+     */
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1(){
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+
+            // 강제 초기화 -> 데이터가 있으니까 실행
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o -> o.getItem().getName());
+
+        }
+        return all;
+    }
+}
