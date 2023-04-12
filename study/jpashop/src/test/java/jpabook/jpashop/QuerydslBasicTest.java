dad0a51f4c9fe2d579292cc6e83efc4c3b34d1ec
suuuinkim <suchae7@naver.com>
2023-04-12 13:49:01 +0900

    [추가] JPQL 과 Querydsl

---
+++ study/jpashop/src/test/java/jpabook/jpashop/QuerydslBasicTest.java
@@ -0,0 +1,79 @@
+package jpabook.jpashop;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jpabook.jpashop.study.datajpa.entity.MemberDataJpa;
+import jpabook.jpashop.study.datajpa.entity.QMemberDataJpa;
+import jpabook.jpashop.study.datajpa.entity.TeamDataJpa;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+public class QuerydslBasicTest {
+
+    @Autowired
+    EntityManager em;
+
+    JPAQueryFactory queryFactory;
+
+    @BeforeEach
+    public void before(){
+        queryFactory = new JPAQueryFactory(em);
+        TeamDataJpa teamA = new TeamDataJpa("teamA");
+        TeamDataJpa teamB = new TeamDataJpa("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        MemberDataJpa member1 = new MemberDataJpa("member1", 10, teamA);
+        MemberDataJpa member2 = new MemberDataJpa("member2", 20, teamA);
+        MemberDataJpa member3 = new MemberDataJpa("member3", 30, teamB);
+        MemberDataJpa member4 = new MemberDataJpa("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    public void startJPQL() throws Exception{
+        //member1
+        String qlString = "select m from MemberDataJpa m " +
+                "where m.username = :username";
+
+        MemberDataJpa findMember = em.createQuery(qlString, MemberDataJpa.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+
+    }
+
+    /**
+     * 1) 컴파일 시점에 오류를 확인할 수 있다
+     * 2) 파라미터 바인딩을 자동으로 해결해준다
+     */
+    @Test
+    public void startQuerydsl() throws Exception{
+        
+        QMemberDataJpa m = new QMemberDataJpa("m");
+
+        MemberDataJpa findMember = queryFactory
+                .select(m)
+                .from(m)
+                .where(m.username.eq("member1")) // 파라미터 바인딩 처리
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+
+    }
+}
