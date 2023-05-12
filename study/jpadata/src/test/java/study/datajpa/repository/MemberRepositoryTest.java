bd580dbaca35e0b8f484acd5c71b6deae216cc47
suuuinkim <suchae7@naver.com>
2023-05-12 23:32:06 +0900

    [추가] 명세

--- study/jpadata/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ study/jpadata/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -224,4 +224,23 @@ class MemberRepositoryTest {
     public void callCustom() throws Exception{
         List<Member> result = memberRepository.findMemberCustom();
     }
+
+    @Test
+    public void specBasic() throws Exception{
+        //given
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+        Member m1 = new Member("m1", 0, teamA);
+        Member m2 = new Member("m2", 0, teamA);
+
+        em.persist(m1);
+        em.persist(m2);
+
+        em.flush();
+        em.clear();
+
+        //when
+        memberRepository.findAll();
+        //then
+    }
 }
