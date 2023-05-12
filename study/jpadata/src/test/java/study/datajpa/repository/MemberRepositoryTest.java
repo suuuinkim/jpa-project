3d63915256304c1c7519284d5e33a1226809c51b
suuuinkim <suchae7@naver.com>
2023-05-12 23:38:20 +0900

    [수정] 명세

--- study/jpadata/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ study/jpadata/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -1,11 +1,13 @@
 package study.datajpa.repository;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.dto.MemberDto;
@@ -240,7 +242,10 @@ class MemberRepositoryTest {
         em.clear();
 
         //when
-        memberRepository.findAll();
+        Specification<Member> spec = MemberSpec.username("m1").and(MemberSpec.teamName("teamA"));
+        List<Member> result = memberRepository.findAll(spec);
+
         //then
+        Assertions.assertThat(result.size()).isEqualTo(1);
     }
 }
